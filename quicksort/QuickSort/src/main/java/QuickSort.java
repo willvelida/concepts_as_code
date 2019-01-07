87f31c4ae5388192b3739800edf1c5a3d64c9aa2
Will Velida <willvelida@hotmail.co.uk>
2019-01-07 18:48:14 +0000

    Adding QuickSort program to repo

--- quicksort/QuickSort/src/main/java/QuickSort.java
+++ quicksort/QuickSort/src/main/java/QuickSort.java
@@ -1,2 +1,47 @@
+import java.util.Arrays;
+
 public class QuickSort {
+
+    public void sort(int[] inputNumbersArray) {
+        sort(inputNumbersArray, 0, inputNumbersArray.length - 1);
+    }
+
+    private void sort(int[] numbers, int start, int end) {
+        if (start < end) {
+            int p = partition(numbers, start, end);
+            sort(numbers, start, p - 1);
+            sort(numbers, p + 1, end);
+        }
+    }
+
+    private int partition(int[] numbers, int start, int end) {
+        int pivot = numbers[end];
+        int x = start - 1;
+        for (int i = start; i < end; i++) {
+            if (numbers[i] < pivot) {
+                x++;
+                swap(numbers, x, i);
+            }
+        }
+        swap(numbers, x + 1, end);
+        return x + 1;
+    }
+
+    private void swap(int[] numbers, int i,int j) {
+        int temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
+    }
+
+    public static void main(String[] args) {
+
+        int[] numbersList = new int[]{6,5,3,4,2,7,8,9,1,2};
+        QuickSort quickSort = new QuickSort();
+
+        quickSort.sort(numbersList);
+
+        System.out.println(Arrays.toString(numbersList));
+
+    }
+
 }
